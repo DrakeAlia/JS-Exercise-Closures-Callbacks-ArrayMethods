fbf1cf7860af48a8e54b08f9d90a03294dad649b
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-03-24 14:57:03 -0700

    completed #1

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(list) {
-  
+function processLength(list, callback) {
+  return callback(list.length)
 }
 
 /**
